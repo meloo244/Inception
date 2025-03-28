3f9f4bdcb739b1dacfb3f456a9a05ebb5e64eeec
Mel <melvynoo143@gmail.com>
2025-03-28 15:43:43 +0100

    setp

---
+++ srcs/requirements/wordpress/tools/setup_wp.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Spécifier la version de WordPress a télécharger
+WP_VERSION="6.7.2-fr_FR"
+
+# Vérifier si WordPress est déjà installé en vérifiant la présence du fichier wp-config.php
+if [ -f ./wp-config.php ]; then
+    echo "WordPress is already downloaded"
+else
+    # Télécharger WordPress si le fichier wp-config.php n'existe pas
+    echo "Downloading WordPress version $WP_VERSION..."
+    curl -O https://fr.wordpress.org/wordpress-$WP_VERSION.tar.gz
+
+    # Extraire les fichiers de WordPress
+    tar xfz wordpress-$WP_VERSION.tar.gz
+
+    # Supprimer l'archive téléchargée pour garder l'image propre
+    rm -rf wordpress-$WP_VERSION.tar.gz
+
+    # Déplacer les fichiers extraits dans le répertoire actuel (si nécessaire)
+    mv wordpress/* .
+
+    # Supprimer le répertoire vide 'wordpress'
+    rm -rf wordpress
+
+echo $MYSQL_USER
+echo $MYSQL_PASSWORD
+echo $MYSQL_HOSTNAME
+echo $MYSQL_DATABASE
+
+Sed est un outil puissant pour manipuler et modifier des fichiers en remplaçant précisément les éléments nécessaires.
+sed -i "s/mmarie/$MYSQL_USER/g" wp-config-sample.php
+sed -i "s/mmarie_pwd42/$MYSQL_PASSWORD/g" wp-config-sample.php
+sed -i "s/mariadb/$MYSQL_HOSTNAME/g" wp-config-sample.php
+sed -i "s/wordpress/$MYSQL_DATABASE/g" wp-config-sample.php
+	cp wp-config-sample.php wp-config.php
+
+##bonus
+
+	wp config set WP_REDIS_HOST redis --allow-root
+  	wp config set WP_REDIS_PORT 6379 --raw --allow-root
+ 	wp config set WP_CACHE_KEY_SALT $DOMAIN_NAME --allow-root
+  	#wp config set WP_REDIS_PASSWORD $REDIS_PASSWORD --allow-root
+ 	wp config set WP_REDIS_CLIENT phpredis --allow-root
+	wp plugin install redis-cache --activate --allow-root
+    wp plugin update --all --allow-root
+	wp redis enable --allow-root
+
+fi
+exec "$@"
